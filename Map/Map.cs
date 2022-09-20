8f27bbb5c00f60244f3cb3ba71447d9d70c8ef18
ilya.volgin <govnihorse@gmail.com>
2022-09-20 17:44:10 +0700

    Add project files.

---
+++ Map/Map.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CosmosStrategy.Map
+{
+    internal class Map
+    {
+    }
+
+    struct Cluster
+    {
+        public int x;
+        public int y;
+        public int radius;
+    }
+}
